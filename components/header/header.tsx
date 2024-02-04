f6ff8eaadabbf9a306a1faedf01ed647b8dcb06d
Abhishek Kumar <www.abhishek090300@gmail.com>
2024-02-04 22:08:20 +0530

    Add-Items-To-Shopping-Cart

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,26 +1,20 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import Menu from './Menu';
 
 const Header = () => {
   return (
     <header>
-        <nav>
-            <div className="navbar justify-between bg-base-300">
-                <Link href="/" className="btn btn-ghost text-lg" >
-                    Next Amazona V2
-                </Link>
-                <ul className="flex">
-                    <li>
-                        <Link className="btn btn-ghost rounded-btn" href="/cart">Cart</Link>
-                    </li>
-                    <li>
-                        <Link className="btn btn-ghost rounded-btn" href="/signin">Sign in</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Next Amazona V2
+          </Link>
+          <Menu />
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
