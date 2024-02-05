1ff033be914df3c60cc622b3c68225c22cd808c5
Abhishek Kumar <www.abhishek090300@gmail.com>
2024-02-06 00:47:38 +0530

    Authenticate-User

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,33 +2,33 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header/header';
+import Providers from '@/components/Providers';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Next Amazona V2',
   description: 'Modern ECommerce Website',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-              <div className="min-h-screen flex flex-col">
-                <Header />
-                {children}
-                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-                  <p>
-                    Copyright © 2023 - All right reserved by Next Amazona V2
-                  </p>
-                </footer>
-              </div>
+        <Providers>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            {children}
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+              <p>Copyright © 2023 - All right reserved by Next Amazona V2</p>
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
-
