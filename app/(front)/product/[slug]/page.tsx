5e4d1c86ebfea5c8278d9e41c705abc92cf08491
Abhishek Kumar <www.abhishek090300@gmail.com>
2024-02-05 23:03:50 +0530

    Load_PRoducts_From_DB

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,14 +1,31 @@
 import AddToCart from '@/components/products/AddToCart';
 import data from '@/lib/data';
+import productService from '@/lib/services/productService';
+import { convertDocToObj } from '@/lib/util';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function ProductDetails({
+export async function generateMetadata({
   params,
 }: {
   params: { slug: string };
 }) {
-  const product = data.product.find((x) => x.slug === params.slug);
+  const product = await productService.getBySlug(params.slug);
+  if (!product) {
+    return { title: 'Product not found' };
+  }
+  return {
+    title: product.name,
+    description: product.description,
+  };
+}
+
+export default async function ProductDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const product = await productService.getBySlug(params.slug);
   if (!product) {
     return <div>Product not found</div>;
   }
@@ -64,7 +81,12 @@ export default function ProductDetails({
               {product.countInStock !== 0 && (
                 <div className="card-actions justify-center">
                   <AddToCart
-                    item={{ ...product, qty: 0, color: '', size: '' }}
+                    item={{
+                      ...convertDocToObj(product),
+                      qty: 0,
+                      color: '',
+                      size: '',
+                    }}
                   />
                 </div>
               )}
