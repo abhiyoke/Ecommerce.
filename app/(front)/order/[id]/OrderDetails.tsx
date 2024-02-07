91fe8a8b0bb73a5a63aef8d44cd4b2c01964b651
Abhishek Kumar <www.abhishek090300@gmail.com>
2024-02-07 11:21:30 +0530

    Create-Order-Details-Page

--- app/(front)/order/[id]/OrderDetails.tsx
+++ app/(front)/order/[id]/OrderDetails.tsx
@@ -1,16 +1,23 @@
 'use client';
-import { useEffect, useState } from 'react';
-import CheckoutSteps from '@/components/CheckoutSteps';
-import useCartService from '@/lib/hooks/useCartStore';
-import { useRouter } from 'next/navigation';
+import { OrderItem } from '@/lib/models/OrderModel';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
-
-import toast from 'react-hot-toast';
-import useSWRMutation from 'swr/mutation';
+import useSWR from 'swr';
 import Image from 'next/image';
 
-const Form = () => {
-  const router = useRouter();
+export default function OrderDetails({
+  orderId,
+  paypalClientId,
+}: {
+  orderId: string;
+  paypalClientId: string;
+}) {
+  const { data: session } = useSession();
+  const { data, error } = useSWR(`/api/orders/${orderId}`);
+
+  if (error) return error.message;
+  if (!data) return 'Loading...';
+
   const {
     paymentMethod,
     shippingAddress,
@@ -19,60 +26,17 @@ const Form = () => {
     taxPrice,
     shippingPrice,
     totalPrice,
-    clear,
-  } = useCartService();
-
-  const { trigger: placeOrder, isMutating: isPlacing } = useSWRMutation(
-    `/api/orders/mine`,
-    async (url) => {
-      const res = await fetch('/api/orders', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          paymentMethod,
-          shippingAddress,
-          items,
-          itemsPrice,
-          taxPrice,
-          shippingPrice,
-          totalPrice,
-        }),
-      });
-      const data = await res.json();
-      if (res.ok) {
-        clear();
-        toast.success('Order placed successfully');
-        return router.push(`/order/${data.order._id}`);
-      } else {
-        toast.error(data.message);
-      }
-    }
-  );
-  useEffect(() => {
-    if (!paymentMethod) {
-      return router.push('/payment');
-    }
-    if (items.length === 0) {
-      return router.push('/');
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [paymentMethod, router]);
-
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return <></>;
+    isDelivered,
+    deliveredAt,
+    isPaid,
+    paidAt,
+  } = data;
 
   return (
     <div>
-      <CheckoutSteps current={4} />
-
+      <h1 className="text-2xl py-4">Order {orderId}</h1>
       <div className="grid md:grid-cols-4 md:gap-5 my-4">
-        <div className="overflow-x-auto md:col-span-3">
+        <div className="md:col-span-3">
           <div className="card bg-base-300">
             <div className="card-body">
               <h2 className="card-title">Shipping Address</h2>
@@ -81,26 +45,24 @@ const Form = () => {
                 {shippingAddress.address}, {shippingAddress.city},{' '}
                 {shippingAddress.postalCode}, {shippingAddress.country}{' '}
               </p>
-              <div>
-                <Link className="btn" href="/shipping">
-                  Edit
-                </Link>
-              </div>
+              {isDelivered ? (
+                <div className="text-success">Delivered at {deliveredAt}</div>
+              ) : (
+                <div className="text-error">Not Delivered</div>
+              )}
             </div>
           </div>
-
           <div className="card bg-base-300 mt-4">
             <div className="card-body">
               <h2 className="card-title">Payment Method</h2>
               <p>{paymentMethod}</p>
-              <div>
-                <Link className="btn" href="/payment">
-                  Edit
-                </Link>
-              </div>
+              {isPaid ? (
+                <div className="text-success">Paid at {paidAt}</div>
+              ) : (
+                <div className="text-error">Not Paid</div>
+              )}
             </div>
           </div>
-
           <div className="card bg-base-300 mt-4">
             <div className="card-body">
               <h2 className="card-title">Items</h2>
@@ -113,7 +75,7 @@ const Form = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {items.map((item) => (
+                  {items.map((item: OrderItem) => (
                     <tr key={item.slug}>
                       <td>
                         <Link
@@ -127,23 +89,16 @@ const Form = () => {
                             height={50}
                           ></Image>
                           <span className="px-2">
-                            {item.name}({item.color} {item.size})
+                            {item.name} ({item.color} {item.size})
                           </span>
                         </Link>
                       </td>
-                      <td>
-                        <span>{item.qty}</span>
-                      </td>
+                      <td>{item.qty}</td>
                       <td>${item.price}</td>
                     </tr>
                   ))}
                 </tbody>
               </table>
-              <div>
-                <Link className="btn" href="/cart">
-                  Edit
-                </Link>
-              </div>
             </div>
           </div>
         </div>
@@ -152,43 +107,30 @@ const Form = () => {
           <div className="card bg-base-300">
             <div className="card-body">
               <h2 className="card-title">Order Summary</h2>
-              <ul className="space-y-3">
+              <ul>
                 <li>
-                  <div className=" flex justify-between">
+                  <div className="mb-2 flex justify-between">
                     <div>Items</div>
                     <div>${itemsPrice}</div>
                   </div>
                 </li>
                 <li>
-                  <div className=" flex justify-between">
+                  <div className="mb-2 flex justify-between">
                     <div>Tax</div>
                     <div>${taxPrice}</div>
                   </div>
                 </li>
                 <li>
-                  <div className=" flex justify-between">
+                  <div className="mb-2 flex justify-between">
                     <div>Shipping</div>
                     <div>${shippingPrice}</div>
                   </div>
                 </li>
                 <li>
-                  <div className=" flex justify-between">
+                  <div className="mb-2 flex justify-between">
                     <div>Total</div>
                     <div>${totalPrice}</div>
                   </div>
-                </li>
-
-                <li>
-                  <button
-                    onClick={() => placeOrder()}
-                    disabled={isPlacing}
-                    className="btn btn-primary w-full"
-                  >
-                    {isPlacing && (
-                      <span className="loading loading-spinner"></span>
-                    )}
-                    Place Order
-                  </button>
                 </li>
               </ul>
             </div>
@@ -197,5 +139,4 @@ const Form = () => {
       </div>
     </div>
   );
-};
-export default Form;
+}
